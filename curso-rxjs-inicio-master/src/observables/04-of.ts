c00119e05b7d2770b0735358c453f433f3cb36b4
Richardson94 <Richcori94@gmail.com>
2023-11-01 23:07:46 -0400

    5th class

---
+++ curso-rxjs-inicio-master/src/observables/04-of.ts
@@ -0,0 +1,22 @@
+import { of } from 'rxjs';
+
+//const obs$ = of(1, 2, 3, 4, 5, 6);
+//const obs$ = of(...[1, 2, 3, 4, 5, 6]);
+//const obs$ = of('data', 1, 'test', 2);
+// const obs$ = of<any>(
+//   [1, 2],
+//   { a: 1, b: 2 },
+//   function () {},
+//   true,
+//   Promise.resolve(true)
+// );
+const obs$ = of<string>('data', 1, 'test', 2);
+
+console.log('Begin d of Obs$');
+obs$.subscribe(
+  (next) => console.log('Next :', next),
+  null,
+  () => console.log('Secuence Completed')
+);
+
+console.log('End of Obs$');
