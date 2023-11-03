6f516f6bf0d3f924a42d32a0beeddaf9f81787d2
Richardson94 <Richcori94@gmail.com>
2023-11-03 19:40:09 -0400

    1st class operators

---
+++ curso-rxjs-inicio-master/src/operators/01-map-pluck-mapTo.ts
@@ -0,0 +1,20 @@
+import { fromEvent, range } from 'rxjs';
+import { map, mapTo, pluck } from 'rxjs/operators';
+
+// range(1, 5)
+//   .pipe(map<number, string>((value) => (value * 10).toString()))
+//   .subscribe(console.log);
+
+const keyup$ = fromEvent<KeyboardEvent>(document, 'keyup');
+
+const keyupCode$ = keyup$.pipe(map((event) => event.code));
+
+//const keyupPluck$ = keyup$.pipe(pluck('key'));
+const keyupPluck$ = keyup$.pipe(pluck('target', 'baseURI'));
+
+const keyupMapTo$ = keyup$.pipe(mapTo('tecla presionada'));
+
+//keyup$.subscribe((val) => console.log('value', val));
+//keyupCode$.subscribe((val) => console.log('map :', val));
+//keyupPluck$.subscribe((val) => console.log('pluck :', val));
+keyupMapTo$.subscribe((val) => console.log('mapTo :', val));
