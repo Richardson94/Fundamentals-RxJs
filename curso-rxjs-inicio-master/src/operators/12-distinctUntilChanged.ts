37438d22691c4e5098dd0100ed554cb95464fa58
Richardson94 <Richcori94@gmail.com>
2023-11-05 23:00:56 -0400

    12th class Operators

--- curso-rxjs-inicio-master/src/operators/12-distinctUntilChanged.ts
+++ curso-rxjs-inicio-master/src/operators/12-distinctUntilChanged.ts
@@ -1,5 +1,9 @@
 import { from, of } from 'rxjs';
-import { distinctUntilChanged } from 'rxjs/operators';
+import { distinct, distinctUntilChanged } from 'rxjs/operators';
+
+const numbers$ = of(1, '1', 1, 3, 3, 3, 2, 2, 3, 3, 4, 3, 1, '1');
+
+//numbers$.pipe(distinctUntilChanged()).subscribe(console.log);
 
 interface Character {
   name: string;
